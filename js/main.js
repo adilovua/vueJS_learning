4f3514ad1ed8d2697dc6698089d5e2035303b74a
u_adilov <u_adilov@mail.ru>
2020-03-25 11:50:03 +0500

    First commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello World!'
+    }
+})
+
+var app1 = new Vue({
+    el: '#app1',
+    data: {
+        message1: 'Hello Everybody!'
+    }
+});
