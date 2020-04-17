f1fcd80d04443405441e0567f8a1b4ae1662764e
u_adilov <u_adilov@mail.ru>
2020-04-17 18:06:49 +0500

    16-04

--- js/main.js
+++ js/main.js
@@ -1,37 +1,76 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello ',
-        toWhom: 'Umid',
-        productName: 'Sock',
-        selectedVariant: 0,
-        href: '#',
-        inventory: 0,
-        details: ["10", "20", "30"],
-        variants: [{
-                variantid: 200,
-                variantcolor: "blue",
-                variantImage: "https://smartshopper.ru/img/aHR0cHM6Ly9jZG4xLm96b25lLnJ1L211bHRpbWVkaWEvMTAzNjY5MjA3MC5qcGc=.jpg",
-                variantsQuontity: 10
-            },
-            {
-                variantid: 201,
-                variantcolor: "green",
-                variantImage: "https://im0-tub-ru.yandex.net/i?id=8600b744ea1011623972727c1256362c&n=33&w=283&h=188",
-                variantsQuontity: 0
-            }
-        ],
-        sizes: ["37", "38", "40"],
-        korzinka: 5,
-        lineOver: { color: 'red', textDecoration: 'line-through' }
+Vue.component('product', {
+    template: `
+        <div>
+            <div class='productInfo'>
+            <h1>{{ hello }}</h1>
+            <a :href="href">This is an anchorHref</a>
+            <p v-if="inventory>10">In Stock</p>
+            <p v-else-if="inventory<=10 && inventory>0">Almost sold out!</p>
+            <p :style=" lineOver " v-else>Out of Stock {{lineOver}}</p>
+            <span v-show='OnSale'>On Sale!</span>
+            <span v-show='!OnSale'>Out of stock!</span>
+            <span v-show='brandSale'>{{ CanSale }}</span>
+            <ul>
+                <li v-for="detail in details ">{{ detail }}</li>
+            </ul>
+            <div v-for="(variant, index) in variants " class="color-box " :style="{ backgroundColor : variant.variantcolor } " @mouseover="ChangeImage(index) ">
+
+            </div>
+            <ul>
+                <li v-for="size in sizes ">{{ size }}</li>
+            </ul>
+
+            <div>
+                <button v-on:click="KorzinkagaQosh" :disabled="!OnSale ">Korzinkaga qoshish</button>
+                <button v-on:click="removefromkorzinka" :disabled="!OnSale ">Korzinkadan olib tashlash!</button>
+
+                <p> Korzinka {{ korzinka }}</p>
+            </div>
+            </div>
+
+            
+        <div class='productImage'>
+              <img v-bind:src="productImg " width="500px " height="500px " alt=" ">
+         </div>
+
+    </div>
+
+    `,
+    data() {
+        return {
+            message: "Hello ",
+            brand: "Umid",
+            productName: "Sorry",
+            selectedVariant: 0,
+            href: "#",
+            inventory: 0,
+            details: ["10", "20", "30"],
+            variants: [{
+                    variantid: 200,
+                    variantcolor: "blue",
+                    variantImage: "https://smartshopper.ru/img/aHR0cHM6Ly9jZG4xLm96b25lLnJ1L211bHRpbWVkaWEvMTAzNjY5MjA3MC5qcGc=.jpg",
+                    variantsQuontity: 10
+                },
+                {
+                    variantid: 201,
+                    variantcolor: "green",
+                    variantImage: "https://im0-tub-ru.yandex.net/i?id=8600b744ea1011623972727c1256362c&n=33&w=283&h=188",
+                    variantsQuontity: 0
+                }
+            ],
+            sizes: ["37", "38", "40"],
+            korzinka: 5,
+            lineOver: { color: "red", textDecoration: "line-through" },
+            brandSale: true
+        }
     },
     methods: {
         KorzinkagaQosh() {
-            this.korzinka += 1
+            this.korzinka += 1;
         },
         ChangeImage(index) {
             this.selectedVariant = index;
-            console.log(index)
+            console.log(index);
         },
         removefromkorzinka() {
             if (this.korzinka > 0) { this.korzinka -= 1; }
@@ -39,13 +78,21 @@ var app = new Vue({
     },
     computed: {
         hello() {
-            return this.message + '  ' + this.toWhom + '!!!';
+            return this.productName + "  " + this.brand + "!!!";
         },
         productImg() {
             return this.variants[this.selectedVariant].variantImage;
         },
         OnSale() {
             return this.variants[this.selectedVariant].variantsQuontity;
+        },
+        CanSale() {
+            return "This " + this.productName + " is made by " + this.brand;
         }
     }
+})
+
+var app = new Vue({
+    el: '#app'
+
 })
